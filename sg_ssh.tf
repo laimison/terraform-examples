0a1bd4a4a7debc864ef595cc483f32dabb8b2f75
laimison <laimison1@gmail.com>
2019-06-12 16:33:22 +0100

    added default and allow_internet security groups to EC2 (default group may help with EFS access)

--- sg_ssh.tf
+++ sg_ssh.tf
@@ -20,6 +20,36 @@ resource "aws_security_group" "allow_ssh_from_everywhere" {
   }
 }
 
+resource "aws_security_group" "allow_internet" {
+  name        = "allow_internet"
+  description = "allow_internet"
+  vpc_id      = "${aws_vpc.example_vpc.id}"
+
+  # Inline rule
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  # Inline rule
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  tags = {
+    Name = "allow_internet"
+  }
+}
+
 ################################################################## DEFAULT VPC ############################################################
 resource "aws_security_group" "allow_ssh_from_everywhere_default" {
   name        = "allow_ssh_from_everywhere_default"
